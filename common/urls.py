8496d01011793e47d82250c3f2a109f5669cdf98
YS31 <yeseong31@naver.com>
2022-01-13 22:58:49 +0900

    0113 add Django login2(normal)

--- common/urls.py
+++ common/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from common.views import signup, signup2
-from kodeal import views
+from .views import *
 
 app_name = 'common'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
 
     # 회원가입
     path('signup/', signup, name='signup'),
@@ -16,4 +15,8 @@ urlpatterns = [
     # Django를 이용한 로그인
     path('login_django/', auth_views.LoginView.as_view(template_name='common/login_django.html'), name='login_django'),
     path('logout_django/', auth_views.LogoutView.as_view(), name='logout_django'),
+
+    # 일반 로그인
+    path('login/', login_main, name='login_main'),
+    path('logout/', logout_main, name='logout_main'),
 ]
--- common/views.py
+++ common/views.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 
+# home
+def index(request):
+    return render(request, 'home.html')
+
+
 # 회뭔가입을 진행하는 함수
 def signup(request):
     if request.method == 'POST':
@@ -43,3 +48,26 @@ def signup2(request):
         # 정상적인 회원가입 확인을 위해 로그인, 로그아웃 기능 추가
         form = UserCreationForm
         return render(request, 'common/signup.html', {'form': form})
+
+
+# 일반 로그인
+def login_main(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            return render(request, 'common/login.html', {'error': 'username 또는 password가 틀렸습니다.'})
+        else:
+            auth.login(request, user)
+            return redirect('/')
+    else:
+        return render(request, 'common/login.html')
+
+
+# 일반 로그아웃
+def logout_main(request):
+    auth.logout(request)
+    return redirect('/')
+
