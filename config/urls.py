e1ac57e8389e17f544c89a54c5d34dd4c1ecef2f
Yeseong31 <yeseong31@naver.com>
2022-01-14 11:59:20 +0900

    0112 add google login

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('kodeal.urls')),  # main page
     path('common/', include('common.urls')),  # common(login)
+    path('google/', include('allauth.urls')),  # common(login)
 ]
